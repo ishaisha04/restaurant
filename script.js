748b4cd3cf2ea76e938592bab94b91bedee0dc6a
isha <isha0409isha@gmail.com>
2023-08-26 23:14:07 +0100

    done

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu=document.getElementById("menu-bar")
+const navbar=document.querySelector(".main-navbar")
+menu.onclick=()=>{
+    navbar.classList.toggle("navopen");
+}
+// const navlink= document.querySelectorAll('.nav_link');
+// const sectionEls= document.querySelectorAll('.section');
+// let currentSection='home'
+// window.addEventListener('scroll',()=>{
+//     sectionEls.forEach(sectionEl=> {
+// if(window.scrollY >= sectionEl.offsetTop){
+//             currentSection=sectionEl.id;
+//         }
+//     });
+//     navlink.forEach(navlink=>{
+//         if(navlink.href.includes(currentSection)){
+//         navlink.classList.add('active');
+//     }
+// });
+// });
